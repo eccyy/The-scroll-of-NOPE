98cba4b65525283408b8b87c01e732ec89f006a0
williamboren <willy.boren@hotmail.com>
2017-03-22 09:33:24 +0100

    Add folder for baseclasses, object.cs file

---
+++ The scroll of NOPE/The scroll of NOPE/BaseClasses/Object.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_scroll_of_NOPE.BaseClasses
+{
+    abstract class Object
+    {
+        protected Texture2D Texture;
+        protected Vector2 Position;
+
+        //public Draw(SpriteBatch sprite, Graphics g)
+        //{
+
+        //}
+    }
+}

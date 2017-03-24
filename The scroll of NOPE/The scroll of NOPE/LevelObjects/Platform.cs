492e83531e46e7c912808471deb6379e9e9f3953
Lucas Haglund <lucas.haglund98@hotmail.com>
2017-03-24 10:35:39 +0100

    Deleted 2 levelObject and made a new one

--- The scroll of NOPE/The scroll of NOPE/LevelObjects/Platform.cs
+++ The scroll of NOPE/The scroll of NOPE/LevelObjects/Platform.cs
@@ -10,7 +10,6 @@ namespace The_scroll_of_NOPE.LevelObjects
 {
     class Platform : LevelObject
     {
-        
         public Platform(Texture2D texture, float X, float Y) : base(texture, X, Y)
         {
             
--- The scroll of NOPE/The scroll of NOPE/LevelObjects/levelObject.cs
+++ The scroll of NOPE/The scroll of NOPE/LevelObjects/levelObject.cs
@@ -3,18 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 
 namespace The_scroll_of_NOPE.LevelObjects
 {
-    abstract class LevelObject : BaseClasses.PhysicalObject
+    class LevelObject
     {
-        public LevelObject(Texture2D texture, float X, float Y)
-        {
-            base.texture = texture;
-            base.position.X = X;
-            base.position.Y = Y;
-        }
     }
 }

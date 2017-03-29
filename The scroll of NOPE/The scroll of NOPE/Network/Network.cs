0ba80ef452847ad40331eff9d1aa0c257d2593b2
williamboren <willy.boren@hotmail.com>
2017-03-29 11:30:09 +0200

    Its a base!

---
+++ The scroll of NOPE/The scroll of NOPE/Network/Network.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace The_scroll_of_NOPE.Network
+{
+    abstract class NetworkBase // Allt i denna fil skrivet av William
+    {
+        protected int Port; // int för vilken nätverksport som ska användas
+        protected string Address; // string för vilken nätverksaddress som ska anslutas till
+        protected TcpClient client; // TcpClient, clienten som används för att skicka data
+    }
+
+    class Node : NetworkBase
+    {
+        // konstruktor, tar en string och en int
+        void Node(string a, int p)
+        {
+            this.Port = p;
+            this.Address = a;
+
+            client = new TcpClient(a, p);
+        }
+    }
+
+    class Host : NetworkBase
+    {
+        private TcpListener listener;
+        private Thread listenerThread;
+
+        void Host(int p, string a)
+        {
+            this.Port = p;
+            this.Address = a;
+
+            client = new TcpClient(a, p);
+            listener = new TcpListener(IPAddress.Any, p);
+
+            ThreadStart start = new ThreadStart(Listener);
+            listenerThread = new Thread(start);
+            listenerThread.IsBackground = true;
+            listenerThread.Start();
+        }
+
+        void Listener()
+        {
+
+        }
+    }
+}

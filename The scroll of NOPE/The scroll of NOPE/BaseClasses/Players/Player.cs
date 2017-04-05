72870b35ad6718a892c4f2420eb2419b3cea3963
Lucas Haglund <lucas.haglund98@hotmail.com>
2017-04-05 09:45:03 +0200

    Gjorde ny jump logik i Player.cs

--- The scroll of NOPE/The scroll of NOPE/BaseClasses/Players/Player.cs
+++ The scroll of NOPE/The scroll of NOPE/BaseClasses/Players/Player.cs
@@ -16,6 +16,7 @@ namespace The_scroll_of_NOPE.BaseClasses.Players
         protected float maxSpeed;
         int mapHeight = 300;
         float jumpHeight = 5;
+        bool hasJumped;
 
         public Player()
         {
@@ -37,13 +38,52 @@ namespace The_scroll_of_NOPE.BaseClasses.Players
             // Rörelsen på spelaren och acceleration på rörelsen.
             keyHandler = Keyboard.GetState();
 
-
-            int jumpTimer = 0;
-
             if (keyHandler.IsKeyDown(Keys.A) && speed.X > -maxSpeed)
                 base.speed.X -= acceleration;
             if (keyHandler.IsKeyDown(Keys.D) && speed.X <  maxSpeed)
                 base.speed.X += acceleration;
+
+
+            if (keyHandler.IsKeyDown(Keys.H))
+                AttackH();
+            if (keyHandler.IsKeyDown(Keys.J))
+                AttackJ();
+            if (keyHandler.IsKeyDown(Keys.K))
+                AttackK();
+            if (keyHandler.IsKeyDown(Keys.L))
+                AttackL();
+            if (keyHandler.IsKeyDown(Keys.H))
+                AttackH();
+
+            #region JumpLogic
+            //Om man trycker space och hasJumped är false (man är inte i luften) så hoppar man.
+            if (keyHandler.IsKeyDown(Keys.Space) && hasJumped == false)
+            {
+                position.Y -= 10f;
+                base.speed.Y = -12f;
+                hasJumped = true;
+            }
+
+            //Om man har hoppat så är detta gravitationen, kan ändras genom att ändra float i.
+            if (hasJumped == true)
+            {
+                float i = 3;
+                base.speed.Y += 0.15f * i;
+            }
+
+            //Om man landar på marken så är hasJumped = false och man kan nu hoppa igen
+            if (position.Y + texture.Height >= 450)
+                hasJumped = false;
+            //Är man på marken finns inget som drar en ner genom mappen
+            if (hasJumped == false)
+                base.speed.Y = 0f;
+            //Ska läggas till
+            //hasJumped = false om man kolliderar med en platform
+            //Går man av en platform ska hasJumped sättas till true
+            #endregion
+
+            #region Jontes hopp, om inte den andra skulle fungera
+            /* Jontes hopp
             // Spelaren hoppar
             if (keyHandler.IsKeyDown(Keys.Space))
                 if (speed.Y == 0 && jumpTimer < jumpHeight)
@@ -55,27 +95,9 @@ namespace The_scroll_of_NOPE.BaseClasses.Players
                 {
                     jumpTimer = 0;
                 }
-                              
-                
-            if (keyHandler.IsKeyDown(Keys.S) && speed.Y <  maxSpeed)
-                base.speed.Y += acceleration;
-            if (keyHandler.IsKeyDown(Keys.H))
-                AttackH();
-            if (keyHandler.IsKeyDown(Keys.J))
-                AttackJ();
-            if (keyHandler.IsKeyDown(Keys.K))
-                AttackK();
-            if (keyHandler.IsKeyDown(Keys.L))
-                AttackL();
-            if (keyHandler.IsKeyDown(Keys.H))
-                AttackH();
-          
+            */
 
-        
-
-
-            base.position += speed;
-
+            /* Jontes Gravitation
             // Gravitationen vid hopp och ett stopp vid map height pixlar ner. 
             if(base.position.Y < mapHeight)
                 base.speed.Y += 1f;
@@ -83,9 +105,11 @@ namespace The_scroll_of_NOPE.BaseClasses.Players
             {
                 base.speed.Y = 0;
             }
+            */
+            #endregion
 
+            base.position += speed;
             base.Update();
-
         }
 
                                                      

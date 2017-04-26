dc39888a006f0b020f2acf929d0aaa31f7e252c1
Lucas Haglund <lucas.haglund98@hotmail.com>
2017-04-26 11:25:10 +0200

    Fixed

--- The scroll of NOPE/The scroll of NOPE/LevelObjects/TheScroll.cs
+++ The scroll of NOPE/The scroll of NOPE/LevelObjects/TheScroll.cs
@@ -18,9 +18,9 @@ namespace The_scroll_of_NOPE.LevelObjects
             this.position = position;
         }
 
-        public void Draw(SpriteBatch spriteBatch, Camera camera)
+        public void Draw(SpriteBatch spriteBatch, Camera camera, GraphicsDevice GD)
         {
-            base.Draw(spriteBatch, camera);
+            base.Draw(spriteBatch, camera, GD);
         }
     }
 }

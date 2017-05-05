64e328649700f548f753e6626db65e581783b54a
Lucas Haglund <lucas.haglund98@hotmail.com>
2017-05-05 11:12:18 +0200

    Removed things

---
+++ The scroll of NOPE/The scroll of NOPE/LevelObjects/HeartPickup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace The_scroll_of_NOPE.LevelObjects
+{
+    class HeartPickup : LevelObject
+    {
+        public HeartPickup(Texture2D texture, Vector2 position, Rectangle hitbox)
+        {
+            this.texture = texture;
+            this.hitbox = hitbox;
+            this.position = position;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Camera camera, GraphicsDevice GD)
+        {
+            base.Draw(spriteBatch, camera, GD);
+        }
+    }
+}

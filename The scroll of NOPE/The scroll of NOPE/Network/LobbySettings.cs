85b0faeee954783a76e6fb9bbd4bcc8981df45fc
williamboren <willy.boren@hotmail.com>
2017-05-10 09:31:02 +0200

    Create LobbySettings class

---
+++ The scroll of NOPE/The scroll of NOPE/Network/LobbySettings.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_scroll_of_NOPE.Network
+{
+    public class LobbySettings
+    {
+        
+    }
+}
